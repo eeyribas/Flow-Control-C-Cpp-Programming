a70860b69d555d9f52d2c75c2a1565b962f4faea
Esen Eyribas <40738927+eeyribas@users.noreply.github.com>
2018-07-08 12:35:01 +0300

    Simple Flow "cin Command"

---
+++ cinCommandFlow.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main(){
+	int x, y;
+
+	cout<<"Iki tamsayiyi toplama= ";
+	cin>>x>>y;
+	cout<<x<<" ve "<<y<<" toplami= "<<(x+y)<<endl;
+
+	getch();
+	return 0;
+}
